2317c3dcba707aabd4d9ac8440ac12059114fca7
ryanhugodev <105669787+ryanhugodev@users.noreply.github.com>
2022-06-29 18:21:13 -0300

    Delete Ergonomia directory

--- Ergonomia/app.js
+++
@@ -1,7 +0,0 @@
-const header = document.querySelector("header");
-
-function stickyNavbar() {
-    header.classList.toggle("scrolled", window.pageYOffset > 0);
-}
-
-window.addEventListener("scroll", stickyNavbar);
